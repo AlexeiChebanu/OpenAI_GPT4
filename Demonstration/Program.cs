e89988d97cb6250fb4b93b060793ea350f64c5c4
Alex <0alexchebanu@gmail.com>
2023-04-05 22:52:36 +0300

    FileStream HttpClient

--- Demonstration/Program.cs
+++ Demonstration/Program.cs
@@ -6,9 +6,3 @@ using OpenAI_Client.Completion;
 using OpenAI_Client.Embedding;
 
 OpenAIClient api = new OpenAIClient("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");
-
-var response = await api.Files.GetFilesAsync();
-foreach (var file in response)
-{
-    Console.WriteLine(file.Name);
-}
